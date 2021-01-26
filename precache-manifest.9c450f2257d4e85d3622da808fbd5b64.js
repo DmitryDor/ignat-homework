fa731b258f0c2ee9bf545960a68517668bdf5753
DmitryDor <dmitrydoroshenko80@gmail.com>
2021-01-26 21:39:37 +0300

    Updates

---
+++ precache-manifest.9c450f2257d4e85d3622da808fbd5b64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7348776a5ee3367930900ea2953c788",
+    "url": "/ignat-homework/index.html"
+  },
+  {
+    "revision": "f8a14ceb27dbc9d042bf",
+    "url": "/ignat-homework/static/css/main.09f93b83.chunk.css"
+  },
+  {
+    "revision": "a5eec3b31ca7866aef3a",
+    "url": "/ignat-homework/static/js/2.490fbe74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ignat-homework/static/js/2.490fbe74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8a14ceb27dbc9d042bf",
+    "url": "/ignat-homework/static/js/main.66352485.chunk.js"
+  },
+  {
+    "revision": "71139a25fb8931d032a2",
+    "url": "/ignat-homework/static/js/runtime-main.3f420c2d.js"
+  }
+]);
